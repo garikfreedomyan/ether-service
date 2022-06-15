80ccc42e8840d0cec1b805b497458f08620672b5
Garik Freedomyan <garikfreedomyan@gmail.com>
2022-06-15 09:39:48 +0300

    configured app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/db';
+import { EtherscanModule } from 'src/etherscan';
+import { BlocksModule } from 'src/blocks';
+import { TransactionsModule } from 'src/transactions';
+import { WalletsModule } from 'src/wallets';
 
 @Module({
   controllers: [],
   providers: [],
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    EtherscanModule,
+    BlocksModule,
+    TransactionsModule,
+    WalletsModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 (async function bootstrap() {
   const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Etherscan service')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
